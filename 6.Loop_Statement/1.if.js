c30e01548ef6e242ba2f7403cc6d2d6239ce45d5
yhpark0728 <yhpark0728@naver.com>
2022-05-22 13:11:05 +0900

    22.05.22 New Directory 6.Loop_Statement added

---
+++ 6.Loop_Statement/1.if.js
@@ -0,0 +1,15 @@
+//조건문 Conditional Statement
+// if(조건){}
+// if(조건){} else{}
+// if(조건){} else fi(조건2) else{}
+let fruit = "orange";
+
+if(fruit === "apple"){
+    console.log("apple")
+}
+else if(fruit === "orange"){
+    console.log("orange");
+}
+else{
+    console.log("not a fruit");
+}
