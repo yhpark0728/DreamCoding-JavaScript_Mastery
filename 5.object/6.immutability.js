04cbe845f859229ae069bb0c593923a1af2c1ef3
yhpark0728 <yhpark0728@naver.com>
2022-05-28 15:38:36 +0900

    22.05.28 first commit version 05.28.1

--- 5.object/6.immutability.js
+++ 5.object/6.immutability.js
@@ -2,14 +2,15 @@
 // 상태변경이 필요한 경우에는, 새로운 상태를(오브젝트, 값) 만들어서 반환해야 함
 // 원시값 - 값에 의한 복사
 // 객체값 - 참조에 의한 복사 (메모리주소)
-function display(num) {
-  num = 5; // ❌
-  console.log(num);
-}
 
-const value = 4;
-display(value);
-console.log(value);
+// function display(num) {
+//   num = 5; // ❌
+//   console.log(num);
+// }
+
+// const value = 4;
+// display(value);
+// console.log(value);
 
 function displayObj(obj) {
   obj.name = 'Park'; // ❌❌❌❌❌❌ 외부로 부터 주어진 인자(오브젝트)를 내부에서 변경 X
