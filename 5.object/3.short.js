4653803bf537ea73482a1b746680f3119bd6b7a8
yhpark0728 <yhpark0728@naver.com>
2022-06-17 14:58:33 +0900

    22.06.17 first commit version 06.17.1

--- 5.object/3.short.js
+++ 5.object/3.short.js
@@ -1,7 +1,7 @@
 const x = 0;
 const y = 0;
 const coordinate = { x, y }; //{ x: x, y: y };
-console.log(coordinate);
+//console.log(coordinate);
 
 //원하는 형태의 object를 생성하는 함수
 function makeObj(name, age) {
